0ded43c8f3b291edfc72ab1f719d8bce359a951e
Olive-Spencer <35235065+Olive-Spencer@users.noreply.github.com>
2018-03-16 00:23:47 -0600

    lesson 10 test

--- assignments/Lesson-9/js/franklinjson.js
+++ assignments/Lesson-9/js/franklinjson.js
@@ -2,6 +2,10 @@ var requestURL= 'https://api.wunderground.com/api/c4def7abb39dd9ac/forecast/cond
 var request= new XMLHttpRequest();
 request.open('GET', requestURL, true);
 request.send();
+var tenDayURL = 'https://api.wunderground.com/api/c4def7abb39dd9ac/forecast10day/q/MN/Franklin.json';
+var tenDayRequest = new XMLHttpRequest();
+tenDayRequest.open('GET', tenDayURL, true);
+tenDayRequest.send();
 
 request.onload = function() {
     var franklin = JSON.parse(request.responseText);
@@ -18,6 +22,21 @@ request.onload = function() {
     document.getElementById('low').innerHTML = franklin.forecast.simpleforecast.forecastday["0"].low.fahrenheit;
     document.getElementById('currentPic').src = franklin.current_observation.icon_url;
     document.getElementById('currentIcon').innerHTML = franklin.current_observation.icon;
+    
+        var tenDay = JSON.parse(tenDayRequest.responseText);
+    console.log(tenDay);
+    document.getElementById('0').innerHTML = tenDay.forecast.simpleforecast.forecastday[0].high.fahrenheit;
+    document.getElementById('1').innerHTML = tenDay.forecast.simpleforecast.forecastday[1].high.fahrenheit; 
+    document.getElementById('2').innerHTML = tenDay.forecast.simpleforecast.forecastday[2].high.fahrenheit; 
+    document.getElementById('3').innerHTML = tenDay.forecast.simpleforecast.forecastday[3].high.fahrenheit; 
+    document.getElementById('4').innerHTML = tenDay.forecast.simpleforecast.forecastday[4].high.fahrenheit; 
+    document.getElementById('5').innerHTML = tenDay.forecast.simpleforecast.forecastday[5].high.fahrenheit; 
+    document.getElementById('6').innerHTML = tenDay.forecast.simpleforecast.forecastday[6].high.fahrenheit; 
+    document.getElementById('7').innerHTML = tenDay.forecast.simpleforecast.forecastday[7].high.fahrenheit; 
+    document.getElementById('8').innerHTML = tenDay.forecast.simpleforecast.forecastday[8].high.fahrenheit; 
+    document.getElementById('9').innerHTML = tenDay.forecast.simpleforecast.forecastday[9].high.fahrenheit; 
+    
+    
     
     
     
